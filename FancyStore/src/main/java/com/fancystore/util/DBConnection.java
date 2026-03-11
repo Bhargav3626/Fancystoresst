cbbc9bd98a29285ff21d590f0fe112e586025cd1
Bhargav3626 <114877935+Bhargav3626@users.noreply.github.com>
2026-03-12 01:36:44 +0530

    Update DBConnection.java
    Done

--- FancyStore/src/main/java/com/fancystore/util/DBConnection.java
+++ FancyStore/src/main/java/com/fancystore/util/DBConnection.java
@@ -8,16 +8,22 @@ public class DBConnection {
     private static Connection con;
 
     public static Connection getConnection() {
+
         try {
+
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/fancystore",
-                "root",
-                "##Qwerty8444"
-            );
-        } catch (Exception e) {
+
+            String url = "jdbc:mysql://centerbeam.proxy.rlwy.net:19596/railway";
+            String username = "root";
+            String password = "IQASTRoHaWdcsvGdnBSEupGQlnWYEKRz";
+
+            con = DriverManager.getConnection(url, username, password);
+
+        } 
+        catch (Exception e) {
             e.printStackTrace();
         }
+
         return con;
     }
 }
